53501419c90db409834e7ee9b50d9c7afc502006
Michal Sänger <michal.sanger@gmail.com>
2014-09-30 23:54:39 +0200

    Basic implementation for inserting points

--- Quadtree/Quadtree.php
+++ Quadtree/Quadtree.php
@@ -4,8 +4,83 @@ namespace Quadtree;
 
 class Quadtree
 {
-    public function insert($item)
+    const CAPACITY = 4;
+
+    /** @var Bounds */
+    private $bounds;
+    
+    /** @var Point[] */
+    private $items = [];
+    
+    /** @var Quadtree */
+    private $nw;
+
+    /** @var Quadtree */
+    private $ne;
+
+    /** @var Quadtree */
+    private $sw;
+
+    /** @var Quadtree */
+    private $se;
+    
+    /**
+     * @param \Quadtree\Bounds $bounds
+     */
+    function __construct(Bounds $bounds)
     {
-        return false;
+        $this->bounds = $bounds;
+    }
+    
+    public function insert(Point $item)
+    {
+        if (!$this->bounds->containsPoint($item)) {
+            return FALSE;
+        }
+        foreach ($this->items as $i) {
+            if ($item->equals($i)) {
+                return FALSE;
+            }
+        }
+        
+        if ($this->nw === NULL && count($this->items) < self::CAPACITY) {
+            $this->items[] = $item;
+            return TRUE;
+        } else {
+            $this->subdivide();
+            $inserted = $this->nw->insert($item) 
+                    || $this->ne->insert($item)
+                    || $this->sw->insert($item)
+                    || $this->se->insert($item);
+            return $inserted;
+        }
+    }
+    
+    private function subdivide()
+    {
+        list($this->nw, $this->ne, $this->sw, $this->se) = $this->getDividedBounds();
+        foreach ($this->items as $item) {
+            $this->nw->insert($item);
+            $this->ne->insert($item);
+            $this->sw->insert($item);
+            $this->se->insert($item);
+        }
+        $this->items = [];
+    }
+    
+    /**
+     * @return Quadtree[]
+     */
+    private function getDividedBounds()
+    {
+        $c = $this->bounds->getCenter();
+        $width = $this->bounds->getWidth() / 2;
+        $height = $this->bounds->getHeight() / 2;
+        
+        $nw = new Quadtree(new Bounds($width, $height, $c->getLeft() - $width, $c->getTop() - $height));
+        $ne = new Quadtree(new Bounds($width, $height, $c->getLeft(), $c->getTop() - $height));
+        $sw = new Quadtree(new Bounds($width, $height, $c->getLeft() - $width, $c->getTop()));
+        $se = new Quadtree(new Bounds($width, $height, $c->getLeft(), $c->getTop()));
+        return [$nw, $ne, $sw, $se];
     }
 }
--- tests/Quadtree.phpt
+++ tests/Quadtree.phpt
@@ -4,7 +4,19 @@ require __DIR__ . '/bootstrap.php';
 
 use Tester\Assert;
 use Quadtree\Quadtree;
+use Quadtree\Bounds;
+use Quadtree\Point;
 
-$qt = new Quadtree();
+$bounds = new Bounds(50, 50);
+$qt = new Quadtree($bounds);
 
-Assert::true(TRUE);
+Assert::true($qt->insert(new Point(10, 10)));
+Assert::false($qt->insert(new Point(10, 10)));
+Assert::false($qt->insert(new Point(100, 100)));
+
+Assert::true($qt->insert(new Point(10, 40)));
+Assert::true($qt->insert(new Point(40, 40)));
+Assert::true($qt->insert(new Point(40, 10)));
+Assert::true($qt->insert(new Point(15, 15)));
+
+//Assert::false($qt->insert(new Point(40, 10)));
