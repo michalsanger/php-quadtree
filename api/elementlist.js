ff162c1710a244d695deb53f7beffa5108233bea
Michal Sänger <michal.sanger@gmail.com>
2014-10-11 23:37:16 +0200

    Update docs

--- api/elementlist.js
+++ api/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Quadtree\\BoundsCollisionDetector"],["c","Quadtree\\Geometry\\Bounds"],["c","Quadtree\\Geometry\\Point"],["c","Quadtree\\ICollisionDetector"],["c","Quadtree\\Insertable"],["c","Quadtree\\Quadtree"],["c","Quadtree\\QuadtreeAbstract"]];
+ApiGen.elements = [["c","Quadtree\\CollisionDetector"],["c","Quadtree\\Geometry\\Bounds"],["c","Quadtree\\Geometry\\Point"],["c","Quadtree\\ICollisionDetector"],["c","Quadtree\\Insertable"],["c","Quadtree\\Quadtree"],["c","Quadtree\\QuadtreeAbstract"]];
